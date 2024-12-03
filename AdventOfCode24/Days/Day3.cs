f0dfddf9048f66f4411d950fd98362c04f4097d7
Thiseas Matthaioudakis <t.matthaioudakis@kaizengaming.com>
2024-12-03 14:10:06 +0200

    Add second part for Day 3

--- AdventOfCode24/Days/Day3.cs
+++ AdventOfCode24/Days/Day3.cs
@@ -9,11 +9,22 @@ namespace AdventOfCode.Days
         {
             var input = ReadText("Day3.txt");
 
-            const string pattern = @"(mul[(]\d{1,3}[,]\d{1,3}[)])";
+            const string commandPattern = @"(mul[(]\d{1,3}[,]\d{1,3}[)])";
+
+            var result = CalculatePart1(input, commandPattern);
+
+            Console.WriteLine($"Sum of all results: {result}");
+
+            CalculatePart2(input, commandPattern);
+        }
+
+        private static void CalculatePart2(string input, string commandPattern)
+        {
+            const string invalidCommandSectionPattern = @"((don't\(\))((.|\n)*?))(?=(don't\(\))|(do\(\))|$)";
+            input = Regex.Replace(input, invalidCommandSectionPattern, string.Empty);
 
             var result = 0;
-
-            foreach (Match command in Regex.Matches(input, pattern))
+            foreach (Match command in Regex.Matches(input, commandPattern))
             {
                 var extractedNumbers = command.Value
                     .Replace("mul(", "")
@@ -25,9 +36,26 @@ namespace AdventOfCode.Days
                 result += extractedNumbers[0] * extractedNumbers[1];
             }
 
-            Console.WriteLine($"Sum of all results: {result}");
+            Console.WriteLine($"Sum of all valid command results: {result}");
+        }
 
+        private static int CalculatePart1(string input, string commandPattern)
+        {
+            var result = 0;
 
+            foreach (Match command in Regex.Matches(input, commandPattern))
+            {
+                var extractedNumbers = command.Value
+                    .Replace("mul(", "")
+                    .Replace(")", "")
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                result += extractedNumbers[0] * extractedNumbers[1];
+            }
+
+            return result;
         }
     }
 }
